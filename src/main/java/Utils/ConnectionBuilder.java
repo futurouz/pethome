dfee08b0834c846e7d1864202ba4eba381af8974
Tatchai Samutsopakul <iridescentz67@gmail.com>
2018-02-06 17:15:34 +0700

    modified connectionBuilder

--- src/main/java/Utils/ConnectionBuilder.java
+++ src/main/java/Utils/ConnectionBuilder.java
@@ -5,10 +5,26 @@
  */
 package Utils;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author azlich
  */
 public class ConnectionBuilder {
+    public static Connection getConnection() throws SQLException{
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://pethome.itbangmod.in.th/users","root","sitagile");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ConnectionBuilder.class.getName()).log(Level.SEVERE, null, ex);
+        }
     
+    return con;
+    }
 }
